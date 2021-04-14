b5ea18f6fe532ad8bf0189999203151e68655a0c
Samuel Bunga <sambunga094@gmail.com>
2021-04-14 10:54:43 -0400

    modified zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,12 @@
 .onLoad <- function(libname, pkgname){
+  home_dir <- path.expand("~")
+  if(dir.exists(paste0(home_dir, '/.fastGene')) == F){
+    print('Creating .fastGene in home directory')
+    dir.create(paste0(home_dir, '/.fastGene/scripts'), recursive = T, 
+               showWarnings = F)
+    print('Downloading scripts...')
+    system(paste0('wget https://github.com/samuelbunga/fastGene/tree/main/scripts/__init__.py -P ', home_dir,'/.fastGene/scripts/__init__.py'))
+    system(paste0('wget https://github.com/samuelbunga/fastGene/tree/main/scripts/mgi_to_hgnc.py -P ', home_dir,'/.fastGene/scripts/mgi_to_hgnc.py'))
+  }
   
 }
